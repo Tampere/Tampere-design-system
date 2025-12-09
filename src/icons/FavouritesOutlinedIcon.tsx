bf02c10aa4afaeb34796a63cd4a648d4e17a4b7b
Harry-Pekka Laakso <harry-pekka.laakso@tampere.fi>
2025-12-09 15:03:40 +0200

    Make default size more easy to change

--- src/icons/FavouritesOutlinedIcon.tsx
+++ src/icons/FavouritesOutlinedIcon.tsx
@@ -8,7 +8,6 @@ export const FavouritesOutlinedIcon = ({
   return (
     <svg
       width={width}
-      height={height}
       viewBox="0 0 24 24"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
--- src/icons/PathGuideIcon.tsx
+++ src/icons/PathGuideIcon.tsx
@@ -1,10 +1,9 @@
 import type { SVGProps } from 'react';
 
-export const PathGuideIcon = ({ width = 24, height = 24, ...props }: SVGProps<SVGSVGElement>) => {
+export const PathGuideIcon = ({ width = 24, ...props }: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       width={width}
-      height={height}
       viewBox="0 0 24 24"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
