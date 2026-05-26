b1e24ffe2a1bba7910bf8a8c928b6524218e783e
minnatur <70951745+minnatur@users.noreply.github.com>
2026-05-26 14:05:51 +0300

    refactor(TREDS-65)!: Remove V2 from themeVariable exports

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,2 @@
 export { vars } from './theme.css.ts';
-export {
-  breakpoints as breakpointsV2,
-  getComponents as getComponentsV2,
-  themeVariables as themeVariablesV2,
-  type ThemeVariables as ThemeVariablesV2,
-} from './themeVariables';
+export { breakpoints, getComponents, themeVariables, type ThemeVariables } from './themeVariables';
