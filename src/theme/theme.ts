8faf954818e3659c52d36d5f640819a0bb74f629
minnatur <70951745+minnatur@users.noreply.github.com>
2026-06-18 15:32:13 +0300

    feat(TREDS-65): Fix variable font setup for mantine and vanilla

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,11 +3,19 @@ import { createTheme } from '@mantine/core';
 // Import mantine core styles
 import '@mantine/core/styles.layer.css';
 
-// Import any global CSS styles
+// Import any other global CSS styles
 import '../styles/index.css';
 
-// theme
+/**
+ * General mantine theme configuration
+ *
+ * Override only necessary values to ensure that mantine uses correct
+ * values by default.
+ */
 export const theme = createTheme({
-  //fontFamily: 'Open Sans Variable, sans-serif',
-  //headings: { fontFamily: 'Montserrat Variable, sans-serif' },
+  fontFamily: 'Open Sans Variable, sans-serif',
+  fontFamilyMonospace: 'Open Sans Variable, sans-serif',
+  headings: {
+    fontFamily: 'Montserrat Variable, sans-serif',
+  },
 });
