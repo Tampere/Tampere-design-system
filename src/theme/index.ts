d3c868df3030a35cd9ff2378cf83fc3bb18e8a9a
minnatur <70951745+minnatur@users.noreply.github.com>
2026-07-28 15:35:08 +0300

    feat(TREDS-65): Replace old theme vars

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,3 @@
-export { vars } from './theme-old.css.ts';
-//export { vars } from './theme.css.ts';
+export { vars } from './theme.css.ts';
 export { getComponents, themeVariables, type ThemeVariables } from './themeVariables';
 export { theme } from './theme.ts';
--- src/theme/theme.css.ts
+++ src/theme/theme.css.ts
@@ -1,15 +1,63 @@
 import { themeToVars } from '@mantine/vanilla-extract';
+import {
+  assignVars,
+  createGlobalTheme,
+  createThemeContract,
+  globalStyle,
+} from '@vanilla-extract/css';
 import { theme } from './theme';
+import { getComponents, themeVariables } from './themeVariables';
 
 // Get mantine CSS variables object
 // Since we are mostly using vanilla variables, most of the default
 // mantine variables can be ignored.
-// Copy necessary variables and functions to the exported vars object.
 const mantineVars = themeToVars(theme);
 
-export const vars = {
-  lightSelector: mantineVars.lightSelector,
-  darkSelector: mantineVars.darkSelector,
-  largerThan: mantineVars.largerThan,
-  smallerThan: mantineVars.smallerThan,
-};
+// Vars created from theme variables
+// Make sure that they behave nicely with mantine variables
+// use vars CSS variables object to access variables in files
+export const vars = createThemeContract(themeVariables);
+createGlobalTheme(':root', vars, themeVariables);
+
+// Use mantine functions smallerThan and largerThan to get
+// consistent breakpoints between mantine and vanilla extract variables
+globalStyle(':root', {
+  '@media': {
+    [mantineVars.smallerThan('xs')]: {
+      vars: assignVars(vars, {
+        ...themeVariables,
+        components: getComponents('xs'),
+      }),
+    },
+    [`(${mantineVars.largerThan('xs')}) and (${mantineVars.smallerThan('sm')})`]: {
+      vars: assignVars(vars, {
+        ...themeVariables,
+        components: getComponents('sm'),
+      }),
+    },
+    [`(${mantineVars.largerThan('sm')}) and (${mantineVars.smallerThan('md')})`]: {
+      vars: assignVars(vars, {
+        ...themeVariables,
+        components: getComponents('md'),
+      }),
+    },
+    [`(${mantineVars.largerThan('md')}) and (${mantineVars.smallerThan('lg')})`]: {
+      vars: assignVars(vars, {
+        ...themeVariables,
+        components: getComponents('lg'),
+      }),
+    },
+    [`(${mantineVars.largerThan('lg')}) and (${mantineVars.smallerThan('xl')})`]: {
+      vars: assignVars(vars, {
+        ...themeVariables,
+        components: getComponents('xl'),
+      }),
+    },
+    [mantineVars.largerThan('xl')]: {
+      vars: assignVars(vars, {
+        ...themeVariables,
+        components: getComponents('xxl'),
+      }),
+    },
+  },
+});
